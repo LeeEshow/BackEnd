3fe913344fa969bd5a055c832eb3480f16bf507c
Eshow Lee <Eshow.Lee@advantech.com>
2025-04-25 08:37:14 +0800

    Update

--- BackEnd/Controllers/Sample.cs
+++ BackEnd/Controllers/Sample.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Net.Http;
 using BackEnd.OnActionHandle;
 using ToolBox.WEB.Struct;
+using ToolBox.WEB;
+using Newtonsoft.Json;
 
 namespace BackEnd.Controllers
 {
@@ -55,5 +57,29 @@ namespace BackEnd.Controllers
             });
         }
 
+        private void EncryptPOST_Client_Sample()
+        {
+            // Client Sample
+
+            WebCryp Client = new WebCryp();
+            // 取得 Server 公鑰
+            var server_key = RESTful.Get(@"https://localhost:44388/Authorize/GetKey");
+
+            // 取得 Token
+            var packet = Client.Encrypt(server_key, new { ID = "Eshow", Password = "A123456" });
+            var res = RESTful.Post<Response>("https://localhost:44388/Authorize/VerifyID", packet);
+            if (!RESTful.Client.DefaultRequestHeaders.Contains("Authorization"))
+            {
+                RESTful.Client.DefaultRequestHeaders.Add("Authorization", "Token " + res.Token);
+            }
+
+            // 加密傳輸
+            packet = Client.Encrypt(server_key, new { ABC = DateTime.Now });
+            var data = RESTful.Post<Response>("https://localhost:44388/Sample/EncryptPOST", packet);
+            packet = JsonConvert.DeserializeObject<Packet>(data.Data.ToString());
+
+            // 解密 Response
+            var str = Client.Decrypt(packet);
+        }
     }
 }
